22d0a2ddeeca93abe60eca67501ff01888ad2caf
Tim Bryan <kabatwa@gmail.com>
2018-12-27 17:38:37 +0000

    adding a few simple tests

--- test/belt.test.js
+++ test/belt.test.js
@@ -4,11 +4,15 @@
 const Belt = require('../belt');
 const config = { 'lanes': 1, 'tileSize': 4 };
 const tileImage = 'image';
+let belt;
+
+beforeEach(() => {
+    belt = new Belt(tileImage, config);
+});
 
 // let tile = new mockTile(1.3721783156506717, 'red');
 
 describe('The belt should', () => {
-    const belt = new Belt(tileImage, config);
 
     test('be setup with some default', () => {
         expect(belt).toEqual({
@@ -20,14 +24,13 @@ describe('The belt should', () => {
             'tileWidth': 4
         });
     });
+
+    test('create a 2D array board with all empty spaces', () => {
+        expect(belt.make2DArray(2, 2)).toEqual([[-1, -1], [-1, -1]]);
+    });
+
+    test.skip('Add another tile to the conveyor belt when required', () => {
+        expect(belt.addNewTile()).toEqual(tile);
+    });
 });
 
-    // test('create a 2D array', () => {
-    //     expect(belt.make2DArray(2, 2)).toEqual([[undefined, undefined], [undefined, undefined]]);
-    // });
-
-    // test('Add another tile to the conveyor belt when required', () => {
-    //     expect(belt.addNewTile()).toEqual(tile);
-    // });
-// });
-// 
