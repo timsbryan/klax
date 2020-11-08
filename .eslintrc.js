1b48e5331d9d9ae8d6c20510d2bf95e446f1bd8d
Tim Bryan <kabatwa@gmail.com>
2020-11-08 22:03:26 +0000

    changed eslintrc and formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,18 +15,11 @@ module.exports = {
         "ecmaVersion": 6,
         "sourceType": "module"
     },
+    "plugins": ["jest"],
     "rules": {
-        "arrow-parens": [
-            "error"
-        ],
-        "brace-style": [
-            "error",
-            "1tbs"
-        ],
-        "comma-dangle": [
-            "error",
-            "only-multiline"
-        ],
+        "arrow-parens": ["error"],
+        "brace-style": ["error", "1tbs"],
+        "comma-dangle": ["error", "only-multiline"],
         "keyword-spacing": [
             "error",
             {
@@ -41,35 +34,23 @@ module.exports = {
                 "ignoreUrls": true
             },
         ],
-        "max-params": [
-            "error",
-            6
-        ],
-        "no-cond-assign": [
-            2,
-            "except-parens"
-        ],
+        "max-params": ["error", 6],
+        "no-cond-assign": [2, "except-parens"],
         "eqeqeq": ["error", "smart"],
-        "no-use-before-define": [
-            2,
-            {
-                "functions": false
-            }
-        ],
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error",
+        "no-use-before-define": [2, {"functions": false}],
         "new-cap": 0,
         "no-caller": 2,
         "no-undef": 2,
         "no-unused-vars": ["error", { "args": "none" }],
         "no-empty": ["error", { "allowEmptyCatch": true }],
         "no-console": "off",
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [ "error", "always" ],
-        "space-before-blocks": [
-            "error",
-            "always"
-        ]
+        "quotes": ["error", "single"],
+        "semi": ["error", "always"],
+        "space-before-blocks": ["error", "always"]
     },
 };
